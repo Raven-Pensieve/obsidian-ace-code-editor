382506f6a42c240948e58038f41e970af26f1b24
RavenHogwarts <fengyj120@163.com>
2026-06-11 22:36:43 +0800

    feat: 增加 Catppuccin 四种主题

---
+++ src/service/themes/catppuccin-latte.ts
@@ -0,0 +1,93 @@
+import * as ace from "ace-builds";
+
+const cssText = `
+.ace-catppuccin-latte .ace_gutter {
+  background: #eff1f5;
+  color: rgb(158, 160, 175);
+}
+
+.ace-catppuccin-latte .ace_print-margin {
+  width: 1px;
+  background: #e8e8e8;
+}
+
+.ace-catppuccin-latte {
+  background-color: #eff1f5;
+  color: #4c4f69;
+}
+
+.ace-catppuccin-latte .ace_cursor {
+  color: #dc8a78;
+}
+
+.ace-catppuccin-latte .ace_marker-layer .ace_selection {
+  background: rgba(124, 127, 147, 0.30);
+}
+
+.ace-catppuccin-latte.ace_multiselect .ace_selection.ace_start {
+  box-shadow: 0 0 3px 0px #eff1f5;
+  border-radius: 2px;
+}
+
+.ace-catppuccin-latte .ace_marker-layer .ace_step {
+  background: rgb(198, 219, 174);
+}
+
+.ace-catppuccin-latte .ace_marker-layer .ace_bracket {
+  margin: -1px 0 0 -1px;
+  border: 1px solid rgb(192, 192, 192);
+}
+
+.ace-catppuccin-latte .ace_marker-layer .ace_active-line {
+  background: #ccd0da;
+}
+
+.ace-catppuccin-latte .ace_gutter-active-line {
+  background-color: #ccd0da;
+}
+
+.ace-catppuccin-latte .ace_marker-layer .ace_selected-word {
+  border: 1px solid rgba(124, 127, 147, 0.30);
+}
+
+.ace-catppuccin-latte .ace_fold {
+    background-color: italic;
+    border-color: #4c4f69;
+}
+.ace-catppuccin-latte .ace_keyword{color:#8839ef;}
+.ace-catppuccin-latte .ace_keyword.ace_operator{color:#179299;}
+.ace-catppuccin-latte .ace_constant.ace_language{color:#d20f39;}
+.ace-catppuccin-latte .ace_constant.ace_numeric{color:#fe640b;}
+.ace-catppuccin-latte .ace_constant.ace_character.ace_escape{color:#ea76cb;}
+.ace-catppuccin-latte .ace_support.ace_function{font-style:italic;
+color:#1e66f5;}
+.ace-catppuccin-latte .ace_support.ace_class{font-style:italic;
+color:#df8e1d;}
+.ace-catppuccin-latte .ace_support.ace_type{font-style:italic;
+color:#df8e1d;}
+.ace-catppuccin-latte .ace_storage.ace_type{color:#8839ef;}
+.ace-catppuccin-latte .ace_string{color:#40a02b;}
+.ace-catppuccin-latte .ace_comment{font-style:italic;
+color:#7c7f93;}
+.ace-catppuccin-latte .ace_variable{font-style:italic;
+color:#1e66f5;}
+.ace-catppuccin-latte .ace_variable.ace_parameter{font-style:italic;
+color:#e64553;}
+.ace-catppuccin-latte .ace_entity.ace_other.ace_attribute-name{color:#df8e1d;}
+.ace-catppuccin-latte .ace_entity.ace_name.ace_function{font-style:italic;
+color:#1e66f5;}
+.ace-catppuccin-latte .ace_entity.ace_name{color:#179299;}
+.ace-catppuccin-latte .ace_entity.ace_name.ace_tag{color:#1e66f5;}
+`;
+
+(ace as any).define("ace/theme/catppuccin-latte-css", ["require", "exports", "module"], function (require: any, exports: any, module: any) {
+	module.exports = cssText;
+});
+
+(ace as any).define("ace/theme/catppuccin-latte", ["require", "exports", "module", "ace/theme/catppuccin-latte-css", "ace/lib/dom"], function (require: any, exports: any, module: any) {
+	exports.isDark = false;
+	exports.cssClass = "ace-catppuccin-latte";
+	exports.cssText = require("./catppuccin-latte-css");
+	var dom = require("../lib/dom");
+	dom.importCssString(exports.cssText, exports.cssClass, false);
+});
---
+++ src/service/themes/catppuccin-mocha.ts
@@ -0,0 +1,93 @@
+import * as ace from "ace-builds";
+
+const cssText = `
+.ace-catppuccin-mocha .ace_gutter {
+  background: #1e1e2e;
+  color: rgb(118, 122, 145);
+}
+
+.ace-catppuccin-mocha .ace_print-margin {
+  width: 1px;
+  background: #e8e8e8;
+}
+
+.ace-catppuccin-mocha {
+  background-color: #1e1e2e;
+  color: #cdd6f4;
+}
+
+.ace-catppuccin-mocha .ace_cursor {
+  color: #f5e0dc;
+}
+
+.ace-catppuccin-mocha .ace_marker-layer .ace_selection {
+  background: rgba(147, 153, 178, 0.25);
+}
+
+.ace-catppuccin-mocha.ace_multiselect .ace_selection.ace_start {
+  box-shadow: 0 0 3px 0px #1e1e2e;
+  border-radius: 2px;
+}
+
+.ace-catppuccin-mocha .ace_marker-layer .ace_step {
+  background: rgb(198, 219, 174);
+}
+
+.ace-catppuccin-mocha .ace_marker-layer .ace_bracket {
+  margin: -1px 0 0 -1px;
+  border: 1px solid rgb(192, 192, 192);
+}
+
+.ace-catppuccin-mocha .ace_marker-layer .ace_active-line {
+  background: #313244;
+}
+
+.ace-catppuccin-mocha .ace_gutter-active-line {
+  background-color: #313244;
+}
+
+.ace-catppuccin-mocha .ace_marker-layer .ace_selected-word {
+  border: 1px solid rgba(147, 153, 178, 0.25);
+}
+
+.ace-catppuccin-mocha .ace_fold {
+    background-color: italic;
+    border-color: #cdd6f4;
+}
+.ace-catppuccin-mocha .ace_keyword{color:#cba6f7;}
+.ace-catppuccin-mocha .ace_keyword.ace_operator{color:#94e2d5;}
+.ace-catppuccin-mocha .ace_constant.ace_language{color:#f38ba8;}
+.ace-catppuccin-mocha .ace_constant.ace_numeric{color:#fab387;}
+.ace-catppuccin-mocha .ace_constant.ace_character.ace_escape{color:#f5c2e7;}
+.ace-catppuccin-mocha .ace_support.ace_function{font-style:italic;
+color:#89b4fa;}
+.ace-catppuccin-mocha .ace_support.ace_class{font-style:italic;
+color:#f9e2af;}
+.ace-catppuccin-mocha .ace_support.ace_type{font-style:italic;
+color:#f9e2af;}
+.ace-catppuccin-mocha .ace_storage.ace_type{color:#cba6f7;}
+.ace-catppuccin-mocha .ace_string{color:#a6e3a1;}
+.ace-catppuccin-mocha .ace_comment{font-style:italic;
+color:#9399b2;}
+.ace-catppuccin-mocha .ace_variable{font-style:italic;
+color:#89b4fa;}
+.ace-catppuccin-mocha .ace_variable.ace_parameter{font-style:italic;
+color:#eba0ac;}
+.ace-catppuccin-mocha .ace_entity.ace_other.ace_attribute-name{color:#f9e2af;}
+.ace-catppuccin-mocha .ace_entity.ace_name.ace_function{font-style:italic;
+color:#89b4fa;}
+.ace-catppuccin-mocha .ace_entity.ace_name{color:#94e2d5;}
+.ace-catppuccin-mocha .ace_entity.ace_name.ace_tag{color:#89b4fa;}
+`;
+
+(ace as any).define("ace/theme/catppuccin-mocha-css", ["require", "exports", "module"], function (require: any, exports: any, module: any) {
+	module.exports = cssText;
+});
+
+(ace as any).define("ace/theme/catppuccin-mocha", ["require", "exports", "module", "ace/theme/catppuccin-mocha-css", "ace/lib/dom"], function (require: any, exports: any, module: any) {
+	exports.isDark = true;
+	exports.cssClass = "ace-catppuccin-mocha";
+	exports.cssText = require("./catppuccin-mocha-css");
+	var dom = require("../lib/dom");
+	dom.importCssString(exports.cssText, exports.cssClass, false);
+});
